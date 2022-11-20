e45456a5b5bc0b1cdbf13add17838c5d955044d3
Yasuohasagi7rank <112807477+Yasuohasagi7rank@users.noreply.github.com>
2022-11-21 01:36:52 +0300

    Add files via upload

---
+++ 10.46/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _10._46
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double storona, storona2;
+            double ugol;
+            double oblast;
+
+            while (true)
+            {
+                Console.Write("Введите значение большей трапеции:");
+                storona = double.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+                Console.Write("Введите значение меньшей трапеции:");
+                storona2 = double.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+                Console.Write("Введите значение угла при большей трапеции:");
+                ugol = double.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+                if (storona <= storona2)
+                    Console.WriteLine("Введите значения еще раз");
+                else
+                    break;
+            }
+
+            oblast = (((storona * storona) - (storona2 * storona2)) * Math.Tan(ugol)) / 4;
+
+            Console.WriteLine("Площадь трапеции = {0:0.00} ", oblast);
+            Console.ReadLine();
+        }
+    }
+
+}
